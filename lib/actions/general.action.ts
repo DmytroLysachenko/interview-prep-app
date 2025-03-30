7e69fd13cef230e6d0c94e183186ae1e74999777
DmytroLysachenko <dlysachenko98@gmail.com>
2025-03-30 20:46:10 +0200

    Changed prompt for generation feedback

--- lib/actions/general.action.ts
+++ lib/actions/general.action.ts
@@ -72,6 +72,8 @@ export const createFeedback = async (params: CreateFeedbackParams) => {
         You are an AI interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories. Be thorough and detailed in your analysis. Don't be lenient with the candidate. If there are mistakes or areas for improvement, point them out.
         Transcript:
         ${formattedTranscript}
+        
+        If there is no transcript provided above - the candidate did not answer any questions, so give evaluation with all 0 scores and recommendations on how to answer questions in interview.
 
         Please score the candidate from 0 to 100 in the following areas. Do not add categories other than the ones provided:
         - **Communication Skills**: Clarity, articulation, structured responses.
