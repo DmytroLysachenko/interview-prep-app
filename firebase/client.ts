377e2f3cfa8cd588e127b5c6057477c7fcdcd38e
DmytroLysachenko <dlysachenko98@gmail.com>
2025-06-20 22:58:05 +0200

    Removed comments

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,4 +1,3 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -13,7 +12,6 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
 
 export const auth = getAuth(app);
